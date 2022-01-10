0d4c897b364a33344d33ac42f848b2f3f98cc5c6
unknown <anhnt.tb12@gmail.com>
2022-01-10 16:48:27 +0700

    tinh tong luy ke

--- vonglapfor.js
+++ vonglapfor.js
@@ -3,8 +3,23 @@ function lap(){
     let text = "";
     let N = Number(document.getElementById("input1").value);
     for (let i = 0; i < N; i++) {
-        text += i;
+        text += "Số "+i + "<br>" ;
     }
     document.getElementById("kq").innerHTML = text;
-    console.log(text);
-}
+    }
+
+    //bài 2
+function totting() {
+    let N2 = Number(document.getElementById("input2").value);
+    total = 0;
+    for ( let j = 1; j <= N2 ; j++)
+    {
+        total = total + j ;
+    }
+    document.getElementById("Sum").innerText = total;
+    }
+// tong=0;
+// for (let j =5;j<=10;j++){
+//     tong=tong+j;
+// }
+// alert ("Tổng các số từ 5 đến 20 là: " + tong);
