9e040feec90587b9f33d51a55e21ad50f60cb989
unknown <anhnt.tb12@gmail.com>
2022-01-10 15:32:16 +0700

    da sưa loi

--- vonglapfor.js
+++ vonglapfor.js
@@ -1,10 +1,10 @@
-let i;
-let text = "";
-let N = Number(document.getElementById("input1").value);
-function lap(){
-    for (i = 0; i < N; i++) {
-        text += i;
-        document.getElementById("kq").innerHTML = text
-    }
 
+function lap(){
+    let text = "";
+    let N = Number(document.getElementById("input1").value);
+    for (let i = 0; i < N; i++) {
+        text += i;
+    }
+    document.getElementById("kq").innerHTML = text;
+    console.log(text);
 }
